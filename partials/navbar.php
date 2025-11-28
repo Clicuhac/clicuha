73a290f087e2a12f48da3459867416371f1dffaf
Clicuhac <superkns701@ukr.net>
2025-11-28 11:32:48 +0200

    Update navbar.php

--- partials/navbar.php
+++ partials/navbar.php
@@ -6,7 +6,7 @@ if (session_status() === PHP_SESSION_NONE) {
 $loggedIn = !empty($_SESSION['user_id'] ?? null);
 $page = basename($_SERVER['PHP_SELF']);
 ?>
-<nav class="navbar navbar-expand-lg bg-white border-bottom mb-4">
+<nav class="navbar navbar-expand-lg bg-white border-bottom mb-4"clic-nav,>
     <div class="container">
         <a class="navbar-brand" href="/index.php">Clicuha</a>
 
