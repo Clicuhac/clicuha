68de58a1d68a84326ef6a6bbbeee0a7eea3afbc0
Clicuhac <superkns701@ukr.net>
2026-08-25 13:03:56 +0300

    feat: complete Russian MVP translations

--- lang/ru.php
+++ lang/ru.php
@@ -1,42 +1,10 @@
 <?php
-
 return [
-
-    // -------- Меню --------
- 'menu.home'      => 'Главная',
-'menu.nick'      => 'Кликухи',
-'menu.about'     => 'О нас',
-'menu.contacts'  => 'Контакты',
-
-    'menu_profile'   => 'Профиль',
-    'menu_settings'  => 'Настройки',
-
-    // -------- Авторизация --------
-    'auth_login'     => 'Войти',
-    'auth_logout'    => 'Выйти',
-    'auth_join'      => 'Регистрация',
-
-    // -------- Заголовки --------
-    'latest_nicknames' => 'Галерея',
-
-    // -------- Статусы --------
-    'status_free'   => 'Свободна',
-    'status_owned'  => 'Уже принадлежит',
-    'creator'       => 'Автор',
-
-    // -------- Кнопки --------
-    'btn_repair'    => 'Починить',
-    'btn_save'      => 'Сохранить',
-    'btn_edit'      => 'Редактировать',
-    'btn_delete'    => 'Удалить',
-
-    // -------- Сообщения --------
-    'msg_error'     => 'Произошла ошибка',
-    'msg_success'   => 'Операция успешно выполнена',
-
-
-
-    'menu.lang.ua'  => 'UA',
-    'menu.lang.en'  => 'EN',
-    'menu.lang.ru'  => 'RU',
+ 'menu.home'=>'Главная','menu.nick'=>'Кликухи','menu.my'=>'Мои кликухи','menu.about'=>'О нас','menu.contacts'=>'Контакты','menu.new'=>'Новая кликуха','menu.cabinet'=>'Кабинет',
+ 'auth_login'=>'Войти','auth_logout'=>'Выйти','auth_join'=>'Регистрация',
+ 'latest_nicknames'=>'Галерея','status_free'=>'Свободна','status_owned'=>'Уже принадлежит','creator'=>'Автор',
+ 'btn_save'=>'Сохранить','btn_edit'=>'Редактировать','btn_delete'=>'Удалить','msg_error'=>'Произошла ошибка','msg_success'=>'Операция успешно выполнена',
+ 'gallery.intro'=>'Галерея творческих кликух. Создавай образы, смотри других и играй с персонажами.','gallery.empty'=>'Пока ничего нет.',
+ 'cabinet.title'=>'Кабинет автора','cabinet.manage'=>'Управление','cabinet.my'=>'Все мои кликухи','cabinet.create'=>'Создать кликуху','cabinet.settings'=>'Настройки','cabinet.theme'=>'Интерьер кабинета',
+ 'cabinet.events'=>'События Clicuha','cabinet.events_text'=>'Здесь появятся баттлы, тусовки и другие события с твоими кликухами.','cabinet.activity'=>'Лента активности','cabinet.activity_text'=>'Здесь будет активность твоих кликух и новости о них.','cabinet.archive'=>'Архив / Библиотека','cabinet.archive_text'=>'Сохранённые истории, иллюстрации, баттлы и другие артефакты Clicuha.','cabinet.ads'=>'Реклама Clicuha Bot Network','cabinet.ads_text'=>'Вертикальная витрина для ботов, партнёрских проектов и событий.','cabinet.archived'=>'Кликуха мягко отправлена в архив.'
 ];
